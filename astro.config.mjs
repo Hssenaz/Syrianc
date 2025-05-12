fb4436c29a1817f839d955ae2f27cbd9a428b85b
Hssenaz <hasoonazawe188@gmail.com>
2025-05-12 11:17:37 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,18 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+sidebar: [
+  {
+    label: 'About',
+    items: [
+      { label: 'What is Syrian Coin?', slug: 'about-syrian-coin' },
+    ],
+  },
+  {
+    label: 'Guides',
+    items: [
+      { label: 'Example Guide', slug: 'guides/example' },
+    ],
+  },
+  {
+    label: 'Reference',
+    autogenerate: { directory: 'reference' },
+  },
+],
