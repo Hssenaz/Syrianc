b36deac4b2eaf4480b4e354fb872f72e24bea5af
Hssenaz <hasoonazawe188@gmail.com>
2025-05-13 00:03:28 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,18 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Syrian Coin',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			customCss: 'src/styles/custom.css',
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/withastro/starlight',
+				},
+			],
 			sidebar: [
 				{
 					label: 'About',
 					items: [
-						// Each item here is one entry in the navigation menu.
 						{ label: 'What is Syrian Coin?', slug: 'about-syrian-coin' },
 					],
 				},
